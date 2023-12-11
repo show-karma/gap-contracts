6957cadbdd783428e5f6b362cf1c9506db3c739d
Mahesh Murthy <mahesh@mmurthy.com>
2023-12-10 20:35:35 -0800

    Extend ownership check to grants in ReferrerResolver

--- contracts/ReferrerResolver.sol
+++ contracts/ReferrerResolver.sol
@@ -14,6 +14,9 @@ contract ReferrerResolver is SchemaResolver, Initializable, OwnableUpgradeable {
     address private _owner;
     IProjectResolver public _projectResolver;
 
+    bytes32 public _grantSchemaUid;
+    bytes32 public _projectSchemaUid;
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor(IEAS eas) SchemaResolver(eas) {
         _disableInitializers();
@@ -24,16 +27,26 @@ contract ReferrerResolver is SchemaResolver, Initializable, OwnableUpgradeable {
         __Ownable_init();
     }
 
-    function refIsProject(bytes32 refSchemaUid) public view returns (bool) {
-        ISchemaRegistry registry = _eas.getSchemaRegistry();
-        SchemaRecord memory schema = registry.getSchema(refSchemaUid);
-        return address(schema.resolver) == address(_projectResolver);
-    }
-
     function setProjectResolver(
         IProjectResolver projectResolver
     ) public onlyOwner {
         _projectResolver = projectResolver;
+    }
+
+    function setGrantSchema(bytes32 grantSchemaUid) public onlyOwner {
+        _grantSchemaUid = grantSchemaUid;
+    }
+
+    function setProjectSchema(bytes32 projectSchemaUid) public onlyOwner {
+        _projectSchemaUid = projectSchemaUid;
+    }
+
+    function refIsProject(bytes32 refSchemaUid) public view returns (bool) {
+        return (refSchemaUid == _projectSchemaUid);
+    }
+
+    function refIsGrant(bytes32 refSchemaUid) public view returns (bool) {
+        return (refSchemaUid == _grantSchemaUid);
     }
 
     function onAttest(
@@ -42,14 +55,16 @@ contract ReferrerResolver is SchemaResolver, Initializable, OwnableUpgradeable {
     ) internal view override returns (bool) {
         if (attestation.refUID != bytes32(0)) {
             Attestation memory ref = _eas.getAttestation(attestation.refUID);
-            require(ref.uid != bytes32(0), "Referred attestation not valid.");
             require(
-                (refIsProject(ref.schema) &&
-                    _projectResolver.isAdmin(ref.uid, attestation.recipient)) ||
-                    ref.attester == attestation.attester ||
-                    ref.recipient == attestation.recipient ||
-                    _owner == attestation.attester,
-                "ReferrerResolver:Not owner"
+              attestation.attester == owner() ||
+              ref.attester == attestation.attester ||
+              ref.recipient == attestation.recipient ||
+              (refIsProject(ref.schema) &&
+                  _projectResolver.isAdmin(ref.uid, attestation.recipient)) ||
+              (refIsGrant(ref.schema) &&
+                  refIsProject(_eas.getAttestation(ref.refUID).schema) &&
+                  _projectResolver.isAdmin(_eas.getAttestation(ref.refUID).uid, attestation.recipient)),
+                "ReferrerResolver:Not admin."
             );
         }
         return true;
